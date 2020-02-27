f407eece2e021e9155bef75eb34e79c3bcf368d1
anmol1812 <33112745+anmol1812@users.noreply.github.com>
2020-02-27 12:05:56 +0530

    Update as.py

--- as.py
+++ as.py
@@ -1,21 +1,13 @@
-import pyaudio
-import wave
-from gtts import gTTS
-import playsound
-import pandas as pd
-import warnings
-warnings.filterwarnings('ignore')
-from pydub import AudioSegment
-from pydub.playback import play
-import speech_recognition as sr
 from googlesearch import search
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
-
-file_audio = sr.AudioFile('output.wav')
-with file_audio as source:
-   audio_text = r.record(source)
-
-print(type(audio_text))
-audio_to_txt = r.recognize_google(audio_text)
-print(audio_to_txt)
+def get_description(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage, "html.parser")
+    title = soup.find("meta",  property="og:description")
+    return(title["content"] if title else "No meta description given")
+query = "games"
+for url in search(query, stop=1):
+    description = get_description(url)
+    print(description)
