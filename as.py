ce37d2d6dcdf66fcee615683bf41701d52e60b35
anmol1812 <33112745+anmol1812@users.noreply.github.com>
2020-02-27 12:11:55 +0530

    Update as.py

--- as.py
+++ as.py
@@ -1,13 +1,17 @@
+# import required packages
 from googlesearch import search
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
+# define connection with the browser and parse html code that show in the developer tools
 def get_description(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
     soup = BeautifulSoup(webpage, "html.parser")
     title = soup.find("meta",  property="og:description")
     return(title["content"] if title else "No meta description given")
-query = "games"
+# take a search query
+query = "gamezop"
+# get text description for the query 
 for url in search(query, stop=1):
     description = get_description(url)
     print(description)
