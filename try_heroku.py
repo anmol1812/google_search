c0b932c494bb442ac51e9c95310bb8442f6db213
anmol1812 <33112745+anmol1812@users.noreply.github.com>
2020-07-09 17:55:55 +0530

    Update try_heroku.py

--- try_heroku.py
+++ try_heroku.py
@@ -17,24 +17,25 @@ def home():
 
 @app.route('/google_search', methods=['POST'])
 def gui_ans():
-    if request.method == 'POST':
-        query = request.args.get('query')
-        def google_scrape(url):
-            page = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-            thepage = urlopen(page).read()
-            #thepage = urllib.request.urlopen(url, headers={'User-Agent': 'Mozilla/5.0'}).read()
-            soup = BeautifulSoup(thepage, "html.parser")
-            #return soup.title.text
-            return soup.title.text
-        
-        i = 1
-        #query = 'gamezop'
-        for url in search(query, stop=1):
-            a = google_scrape(url)
-            print(str(i) + ". " + a)
-            #print(url)
-            print(" ")
-            i += 1
+    #if request.method == 'POST':
+        #query = request.args.get('query')
+    query = request.form.values('')
+    def google_scrape(url):
+        page = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        thepage = urlopen(page).read()
+        #thepage = urllib.request.urlopen(url, headers={'User-Agent': 'Mozilla/5.0'}).read()
+        soup = BeautifulSoup(thepage, "html.parser")
+        #return soup.title.text
+        return soup.title.text
+
+    i = 1
+    #query = 'gamezop'
+    for url in search(query, stop=1):
+        a = google_scrape(url)
+        print(str(i) + ". " + a)
+        #print(url)
+        print(" ")
+        i += 1
             
     return a
 
