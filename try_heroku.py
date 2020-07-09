34c775b6948f09f28d90d2e907bc6a6f30a62f7e
anmol1812 <33112745+anmol1812@users.noreply.github.com>
2020-07-09 16:27:25 +0530

    Add files via upload

---
+++ try_heroku.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 17:57:48 2020
+
+@author: Anmol
+"""
+"""
+function addhttp($url) {
+    if (!preg_match("~^(?:f|ht)tps?://~i", $url)) {
+        $url = "http://" . $url;
+    }
+    return $url;
+}
+"""
+
+import importlib
+#google = importlib.import_module('/usr/local/lib/python2.7/site-packages/google/__init__.py')
+from googlesearch import search
+from flask import Flask, request, redirect, url_for, flash, jsonify, Response
+import json
+import os
+import flask
+import urllib
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+
+app = Flask(__name__)
+
+@app.route('/google_search', methods=['POST'])
+def gui_ans():
+    if request.method == 'POST':
+        query = request.args.get('query')
+        def google_scrape(url):
+            page = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            thepage = urlopen(page).read()
+            #thepage = urllib.request.urlopen(url, headers={'User-Agent': 'Mozilla/5.0'}).read()
+            soup = BeautifulSoup(thepage, "html.parser")
+            #return soup.title.text
+            return soup.title.text
+        
+        i = 1
+        #query = 'gamezop'
+        for url in search(query, stop=1):
+            a = google_scrape(url)
+            print(str(i) + ". " + a)
+            #print(url)
+            print(" ")
+            i += 1
+            
+    return a
+
+if __name__ == '__main__':
+    app.run()
